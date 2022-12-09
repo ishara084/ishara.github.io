a3701f55e4f0ac339ad464b6549ed6d0f418c2a3
Dhananjaya <idhananjay@wiley.com>
2022-12-09 23:23:18 +0530

    readme

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -3,6 +3,7 @@ import {Fade} from "react-reveal";
 import emoji from "react-easy-emoji";
 import "./Greeting.scss";
 import landingPerson from "../../assets/lottie/landingPerson";
+// import yogaDeveloper from "../../assets/lottie/yogaDeveloper";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import SocialMedia from "../../components/socialMedia/SocialMedia";
 import Button from "../../components/button/Button";
